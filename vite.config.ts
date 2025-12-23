481a4014fa1693a2ccbba6f1ae6a62021d7856bb
fiibtor <fiibtor@proton.me>
2025-12-23 11:03:26 +0100

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/caucgen/",
+  base: "https://fiibtor.github.io/caucgen/",
   optimizeDeps: {
     exclude: ["magick-wasm"]
   },
